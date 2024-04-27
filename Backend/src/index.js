cb5fe701591e0617b132071c2d27a640842fe26c
Zaheer Ahmad <zaheerahmadlangrial92@gmail.com>
2024-04-27 15:38:40 +0500

    Connect to Db and make book api

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import bookRoute from './route/book.route.js'
 
 const app = express()
 dotenv.config()
@@ -7,12 +9,24 @@ dotenv.config()
 const PORT = process.env.PORT || 3600;
 
 
-app.get('/' ,(req , res) => {
-    res.send('BookStore App By Author Zaheer Ahmad') 
-})
+try {
+    mongoose.connect(process.env.DBURL)
+    console.log('Database connected......')
+} catch (error) {
+    console.log(error)
+}
+// mongoose.connect(process.env.DBURL).then(() => {
+//     console.log('Database Connected......')
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+//Define Routes
+
+app.use('/book' , bookRoute)
 
 
 
 app.listen(PORT , () => {
-    console.log(`Server is running on this port 3500 ${PORT}`)
+    console.log(`Server is running on this port ${PORT}`)
 })
